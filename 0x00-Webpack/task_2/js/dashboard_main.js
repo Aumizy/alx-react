573de3c27a40899289b391f4a000d906788dc5ef
aumizy <aumizy01@gmail.com>
2024-11-15 08:21:35 +0000

    0x00

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import '../css/main.css';
+import $ from 'jquery';
+import _ from 'lodash';
+import logo from '../assets/holberton-logo.jpg';
+
+$('body').append("<div id='logo'> </div>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter () {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
+$('#logo').css('background-image', `url(${logo})`);
